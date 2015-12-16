d5d8565ce819008dc6f37be6c4def4769d15cd80
Jim Laney <jlaney@uw.edu>
2015-12-16 11:24:03 -0800

    adds import_courses, monitor_imports to jobs

--- sis_provisioner/management/commands/monitor_imports.py
+++ sis_provisioner/management/commands/monitor_imports.py
@@ -1,9 +1,9 @@
-from django.core.management.base import BaseCommand, CommandError
+from sis_provisioner.management.commands import SISProvisionerCommand
 from sis_provisioner.pidfile import Pidfile, ProcessRunningException
 from sis_provisioner.models import Import
 
 
-class Command(BaseCommand):
+class Command(SISProvisionerCommand):
     help = "Monitors the status of sis imports to Canvas."
 
     def handle(self, *args, **options):
@@ -11,5 +11,6 @@ class Command(BaseCommand):
             with Pidfile():
                 for imp in Import.objects.filter(canvas_id__isnull=False):
                     imp.update_import_status()
+                self.update_job()
         except ProcessRunningException as err:
             pass
