27a6fea2871f8e37e526b2a503f3c7664b74fece
mike seibel <mikes@washington.edu>
2016-01-25 14:51:01 -0800

    adds kws cache policy

--- sis_provisioner/cache.py
+++ sis_provisioner/cache.py
@@ -5,6 +5,10 @@ import re
 class RestClientsCache(TimedCache):
     """ A custom cache implementation for Canvas """
 
+    canvas_url_roles = '/api/v1/accounts/%s/roles'
+    kws_url_current_key = '/key/v1/type/%s/encryption/current'
+    kws_url_key = '/key/v1/encryption/%s.json'
+
     url_policies = {}
     url_policies["sws"] = (
         (re.compile(r"^/student/v5/term/"), 60 * 60 * 10),
@@ -15,9 +19,28 @@ class RestClientsCache(TimedCache):
         (re.compile(r"^/identity/v1/entity/"), 60 * 60),
         #(re.compile(r"^/idcard/v1/photo/"), 60 * 60 * 24 * 7),
     )
-    url_policies["canvas"] = (
-        (re.compile(r"^/api/v1/accounts/\d+/roles"), 60 * 60 * 4),
+    url_policies["kws"] = (
+        (re.compile(r"^%s" % (
+            kws_url_key % '[\-\da-fA-F]{36}\\')), 60 * 60 * 24 * 30),
+        (re.compile(r"^%s" % (
+            kws_url_current_key % "[\-\da-zA-Z]+")), 60 * 60 * 24 * 7),
     )
+    url_policies["canvas"] = (
+        (re.compile(r"^%s" % (canvas_url_roles % '\d+')), 60 * 60 * 4),
+    )
+
+    def deleteCache(self, service, url):
+        try:
+            entry = CacheEntryTimed.objects.get(service=service, url=url)
+            entry.delete()
+        except CacheEntryTimed.DoesNotExist:
+            return
+
+    def delete_cached_kws_current_key(self, resource_type):
+        self.deleteCache('kws', self.kws_url_current_key % resource_type)
+
+    def delete_cached_kws_key(self, key_id):
+        self.deleteCache('kws', self.kws_url_current_key % resource_type)
 
     def _get_cache_policy(self, service, url):
         for policy in RestClientsCache.url_policies.get(service, []):
