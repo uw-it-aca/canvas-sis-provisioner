95bc0b3b5a70ec4846114cb3fbf4573e55908830
Jim Laney <jlaney@uw.edu>
2016-02-05 16:00:31 -0800

    roll back inactive status

--- sis_provisioner/csv_formatters.py
+++ sis_provisioner/csv_formatters.py
@@ -91,10 +91,7 @@ def csv_for_sis_student_enrollment(registration):
     if registration.is_active:
         status = Enrollment.ACTIVE_STATUS
     else:
-        if datetime.now() > registration.section.term.get_eod_census_day():
-            status = Enrollment.INACTIVE_STATUS
-        else:
-            status = Enrollment.DELETED_STATUS
+        status = Enrollment.DELETED_STATUS
 
     return _csv_for_enrollment(registration.section.canvas_section_sis_id(),
         registration.person, role, status)
