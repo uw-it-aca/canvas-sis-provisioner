3546c94c058edd96b51df2dae899edfd09a967ff
Jim Laney <jlaney@uw.edu>
2018-10-01 13:32:44 -0700

    django2.1 compatible travis files

--- sis_provisioner/urls.py
+++ sis_provisioner/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url, include
+from django.urls import re_path
 from django.views.generic.base import TemplateView
 from sis_provisioner.views.admin import (
     ImportStatus, ManageUsers, ManageCourses, ManageGroups, ManageAdmins,
@@ -19,43 +19,43 @@ from sis_provisioner.views.external_tools import (
 
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='index.html')),
-    url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt',
-                                               content_type='text/plain')),
-    url(r'^admin/?$', ImportStatus, name='ImportStatus'),
-    url(r'^admin/users$', ManageUsers, name='ManageUsers'),
-    url(r'^admin/courses$', ManageCourses, name='ManageCourses'),
-    url(r'^admin/groups$', ManageGroups, name='ManageGroups'),
-    url(r'^admin/admins$', ManageAdmins, name='ManageAdmins'),
-    url(r'^admin/jobs$', ManageJobs, name='ManageJobs'),
-    url(r'^admin/external_tools$', ManageExternalTools,
-        name='ManageExternalTools'),
+    re_path(r'^$', TemplateView.as_view(template_name='index.html')),
+    re_path(r'^robots\.txt$', TemplateView.as_view(
+        template_name='robots.txt', content_type='text/plain')),
+    re_path(r'^admin/?$', ImportStatus, name='ImportStatus'),
+    re_path(r'^admin/users$', ManageUsers, name='ManageUsers'),
+    re_path(r'^admin/courses$', ManageCourses, name='ManageCourses'),
+    re_path(r'^admin/groups$', ManageGroups, name='ManageGroups'),
+    re_path(r'^admin/admins$', ManageAdmins, name='ManageAdmins'),
+    re_path(r'^admin/jobs$', ManageJobs, name='ManageJobs'),
+    re_path(r'^admin/external_tools$', ManageExternalTools,
+            name='ManageExternalTools'),
 
     # API urls
-    url(r'api/v1/canvas$', CanvasStatus.as_view()),
-    url(r'api/v1/canvas/course/(?P<sis_id>[a-zA-Z0-9 &-]+)$',
-        CanvasCourseView.as_view()),
-    url(r'api/v1/canvas/account/(?P<account_id>[0-9]+)$',
-        CanvasAccountView.as_view()),
-    url(r'api/v1/course/(?P<course_id>[a-zA-Z0-9\-_ &]+)$',
-        CourseView.as_view()),
-    url(r'api/v1/courses/?$', CourseListView.as_view()),
-    url(r'api/v1/users/?$', UserView.as_view()),
-    url(r'api/v1/import/(?P<import_id>[0-9]+)?$', ImportView.as_view()),
-    url(r'api/v1/imports/?$', ImportListView.as_view()),
-    url(r'api/v1/groups/?$', GroupListView.as_view()),
-    url(r'api/v1/terms/?$', TermListView.as_view()),
-    url(r'api/v1/enrollments/?$', EnrollmentListView.as_view()),
-    url(r'api/v1/events/?(?P<begin>[0-9\-:TZtz]+)'
-        r'?(?P<end>[0-9\-:TZtz]+)'
-        r'?(?P<on>[0-9\-:TZtz]+)'
-        r'?(?P<type>\w+)?$', EventListView.as_view()),
-    url(r'api/v1/admins/?$', AdminSearch.as_view()),
-    url(r'api/v1/accounts/?$', AccountSearch.as_view()),
-    url(r'api/v1/soc/?$', AccountSoC.as_view()),
-    url(r'api/v1/job/(?P<job_id>[0-9]+)?$', JobView.as_view()),
-    url(r'api/v1/jobs/?$', JobListView.as_view()),
-    url(r'api/v1/external_tool/(?P<canvas_id>[0-9]+)?$',
-        ExternalToolView.as_view()),
-    url(r'api/v1/external_tools/?$', ExternalToolListView.as_view()),
+    re_path(r'api/v1/canvas$', CanvasStatus.as_view()),
+    re_path(r'api/v1/canvas/course/(?P<sis_id>[a-zA-Z0-9 &-]+)$',
+            CanvasCourseView.as_view()),
+    re_path(r'api/v1/canvas/account/(?P<account_id>[0-9]+)$',
+            CanvasAccountView.as_view()),
+    re_path(r'api/v1/course/(?P<course_id>[a-zA-Z0-9\-_ &]+)$',
+            CourseView.as_view()),
+    re_path(r'api/v1/courses/?$', CourseListView.as_view()),
+    re_path(r'api/v1/users/?$', UserView.as_view()),
+    re_path(r'api/v1/import/(?P<import_id>[0-9]+)?$', ImportView.as_view()),
+    re_path(r'api/v1/imports/?$', ImportListView.as_view()),
+    re_path(r'api/v1/groups/?$', GroupListView.as_view()),
+    re_path(r'api/v1/terms/?$', TermListView.as_view()),
+    re_path(r'api/v1/enrollments/?$', EnrollmentListView.as_view()),
+    re_path(r'api/v1/events/?(?P<begin>[0-9\-:TZtz]+)'
+            r'?(?P<end>[0-9\-:TZtz]+)'
+            r'?(?P<on>[0-9\-:TZtz]+)'
+            r'?(?P<type>\w+)?$', EventListView.as_view()),
+    re_path(r'api/v1/admins/?$', AdminSearch.as_view()),
+    re_path(r'api/v1/accounts/?$', AccountSearch.as_view()),
+    re_path(r'api/v1/soc/?$', AccountSoC.as_view()),
+    re_path(r'api/v1/job/(?P<job_id>[0-9]+)?$', JobView.as_view()),
+    re_path(r'api/v1/jobs/?$', JobListView.as_view()),
+    re_path(r'api/v1/external_tool/(?P<canvas_id>[0-9]+)?$',
+            ExternalToolView.as_view()),
+    re_path(r'api/v1/external_tools/?$', ExternalToolListView.as_view()),
 ]
