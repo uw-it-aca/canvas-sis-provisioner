79b68d27af1551270389cdd5fbc1c242566a6178
Jim Laney <jlaney@uw.edu>
2019-03-04 12:45:57 -0800

    display canvas_id for root account

--- sis_provisioner/static/canvas_admin/js/main.js
+++ sis_provisioner/static/canvas_admin/js/main.js
@@ -1042,7 +1042,7 @@ $(document).ready(function () {
                         context.admins.push({
                             net_id: admin.net_id,
                             role: admin.role,
-                            account_id: admin.account.sis_id,
+                            account_id: admin.account.sis_id || admin.account.canvas_id,
                             account_link: admin.account.canvas_url,
                             added_date: admin.added_date,
                             is_deleted: admin.is_deleted
