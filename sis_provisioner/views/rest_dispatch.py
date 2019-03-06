ac7039e793e1c31c9279cb336bcafe156fd33296
Jim Laney <jlaney@uw.edu>
2019-03-06 15:36:23 -0800

    hotfix for SoC api

--- sis_provisioner/views/rest_dispatch.py
+++ sis_provisioner/views/rest_dispatch.py
@@ -9,13 +9,15 @@ import json
 @method_decorator(group_required(settings.CANVAS_MANAGER_ADMIN_GROUP),
                   name='dispatch')
 class RESTDispatch(View):
-    def error_response(self, status, message='', content={}):
+    @staticmethod
+    def error_response(status, message='', content={}):
         content['error'] = '{}'.format(message)
         return HttpResponse(json.dumps(content),
                             status=status,
                             content_type='application/json')
 
-    def json_response(self, content='', status=200):
+    @staticmethod
+    def json_response(content='', status=200):
         return HttpResponse(json.dumps(content, sort_keys=True),
                             status=status,
                             content_type='application/json')
