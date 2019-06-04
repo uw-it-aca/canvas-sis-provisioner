e77bc133a39d3c314e32b98dc70da9fa9c027595
Jim Laney <jlaney@uw.edu>
2019-06-04 15:17:14 -0700

    one more update to uw-groups-lti

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         'django-blti>=2.1.2,<3.0',
         'django-aws-message>=1.5.1',
         'UW-Canvas-Users-LTI>=0.8,<1.0',
-        'UW-Groups-LTI>=0.7,<1.0',
+        'UW-Groups-LTI>=0.7.1,<1.0',
         'UW-Course-Roster-LTI>=1.0,<2.0',
         'UW-Grading-Standard-LTI>=0.8,<1.0',
         'UW-Library-Guides-LTI>=0.6,<1.0',
