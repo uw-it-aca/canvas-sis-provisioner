6f485d09b76803e65a54944376aca5652cecbd6c
Jim Laney <jlaney@uw.edu>
2019-06-05 16:19:44 -0700

    adds title abbr

--- sis_provisioner/dao/__init__.py
+++ sis_provisioner/dao/__init__.py
@@ -6,15 +6,15 @@ import re
 RE_WORD_BOUNDS = re.compile(r'(\s|-|\(|\)|\.|,|/|:|&|")')
 RE_TITLE_ABBR = re.compile(
     r'^(?:'
-    r'3d|3d4m|Aids|Anmc|Apca|Asp|Basw|'
+    r'3d|3d4m|Acms|Aids|Anmc|Apc|Apca|Asp|Basw|'
     r'Cad|Cam|Cep|Cisb|Cma|Cophp|Cse|Csr|Css|Css3|Csss|Ct|'
     r'Dna|Dsm|Dub|Edp|Ehr|Fda|Gh|Ghc|Gis|Gix|'
     r'Hcde|Hci|Hi|Hihim|Hiv|Hmc|Hr|Html5|'
-    r'Ias|Ibep|Icd|Ielts|Id|Ii|Iii|Ios|It|Iv|Jsis|'
+    r'Ias|Ibep|Icd|Ielts|Id|Ii|Iii|Ios|Ip|It|Iv|Ix|Jsis|'
     r'Lgbt|Lgbtq|Llm|Mpa|Mph|Msis|Msw|Mt|Napm|Otc|'
     r'Rcs|Rf|Rotc|Scca|Sql|Sti|Ta|Toefl|Tv|'
     r'Uh|Us|Uw|Uwb|Uwcr|Uweo|Uwmc|Uwt|'
-    r'Va|Vamc|Vba|Vi|Wa|Wsma|Wwami|Wy|Xml'
+    r'Va|Vamc|Vba|Vi|Vii|Viii|Wa|Wsma|Wwami|Wy|Xml'
     r')$')
 
 
