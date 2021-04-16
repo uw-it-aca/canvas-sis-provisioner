5d970860a7596cfac92c0089efb7d4425e5a86e5
Jim Laney <jlaney@uw.edu>
2021-04-16 08:39:14 -0700

    add CommandError to monitor_imports

--- sis_provisioner/management/commands/monitor_imports.py
+++ sis_provisioner/management/commands/monitor_imports.py
@@ -1,6 +1,7 @@
 # Copyright 2021 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
 
+from django.core.management.base import CommandError
 from sis_provisioner.management.commands import SISProvisionerCommand
 from sis_provisioner.models import Import
 from logging import getLogger
@@ -16,5 +17,6 @@ class Command(SISProvisionerCommand):
             for imp in Import.objects.find_by_requires_update():
                 imp.update_import_status()
             self.update_job()
-        except Exception as ex:
-            logger.error("{}".format(ex))
+        except Exception as err:
+            logger.error("{}".format(err))
+            raise CommandError(err)
