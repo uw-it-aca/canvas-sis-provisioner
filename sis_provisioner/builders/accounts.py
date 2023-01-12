6273bba610d01a2ea4d6c20397459b3463cd83bc
Jim Laney <jlaney@uw.edu>
2023-01-12 12:18:53 -0800

    update copyright

--- sis_provisioner/builders/accounts.py
+++ sis_provisioner/builders/accounts.py
@@ -1,5 +1,6 @@
-# Copyright 2022 UW-IT, University of Washington
+# Copyright 2023 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
+
 
 from sis_provisioner.builders import Builder
 from sis_provisioner.models.account import Curriculum
