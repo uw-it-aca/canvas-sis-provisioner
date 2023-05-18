d17894248ab6c0a0f6b31b095c4812f53a5404c2
Jim Laney <jlaney@uw.edu>
2023-05-18 15:14:47 -0700

    refactor queue_active_courses job, resolves CVS-824, CVS-827, CVS-829

--- sis_provisioner/management/commands/queue_active_courses.py
+++ sis_provisioner/management/commands/queue_active_courses.py
@@ -2,8 +2,9 @@
 # SPDX-License-Identifier: Apache-2.0
 
 
+from django.conf import settings
 from sis_provisioner.management.commands import SISProvisionerCommand
-from sis_provisioner.dao.term import get_current_active_term
+from sis_provisioner.dao.term import get_all_active_terms
 from sis_provisioner.models.course import Course
 
 
@@ -11,6 +12,10 @@ class Command(SISProvisionerCommand):
     help = "Re-queue active courses for the current term."
 
     def handle(self, *args, **options):
-        term = get_current_active_term()
-        Course.objects.prioritize_active_courses_for_term(term)
+        priority_expires_week = getattr(settings, 'PRIORITY_EXPIRES_WEEK', 5)
+        for term in get_all_active_terms():
+            if term.get_week_of_term() > priority_expires_week:
+                continue
+            Course.objects.prioritize_active_courses_for_term(term)
+
         self.update_job()
