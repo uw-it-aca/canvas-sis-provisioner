1de67c1c7df70b3553b2877e5d6d8f3a434a2a2c
Jim Laney <jlaney@uw.edu>
2024-01-16 14:58:33 -0800

    backfill courses from report

--- sis_provisioner/management/commands/backfill_courses.py
+++ sis_provisioner/management/commands/backfill_courses.py
@@ -3,17 +3,9 @@
 
 
 from django.core.management.base import BaseCommand, CommandError
-from django.core.files.storage import default_storage
-from django.db.utils import IntegrityError
-from django.utils.timezone import utc
-from sis_provisioner.dao.canvas import update_course_sis_id
-from sis_provisioner.dao.course import adhoc_course_sis_id
+from sis_provisioner.dao.canvas import get_course_report_data, get_course_by_id
 from sis_provisioner.models.course import Course
-from uw_canvas.terms import Terms
-from dateutil.parser import parse
-from dateutil.parser._parser import ParserError
 from logging import getLogger
-import codecs
 import csv
 
 logger = getLogger(__name__)
@@ -23,7 +15,7 @@ class Command(BaseCommand):
     help = "Insert courses from a Canvas course provisioning report file"
 
     def add_arguments(self, parser):
-        parser.add_argument('course_file', help='Course file path')
+        parser.add_argument('-t', '--term-sis-id', help='Term SIS ID')
         parser.add_argument('-c', '--commit', action='store_true',
                             dest='commit', default=False,
                             help='Insert/update course models')
@@ -34,61 +26,66 @@ class Command(BaseCommand):
             f'SIS ID: {course.course_id}, Created: {course.created_date}, '
             f'Expires: {course.expiration_date}')
 
-    def get_all_terms(self):
-        terms = {}
-        for term in Terms().get_all_terms():
-            terms[str(term.term_id)] = term.sis_term_id or 'default'
-        return terms
-
     def handle(self, *args, **options):
+        term_sis_id = options.get('term-sis-id')
         commit = options.get('commit')
-        course_file = options.get('course_file')
-        terms = self.get_all_terms()
 
-        with default_storage.open(course_file, mode='r') as csvfile:
-            reader = csv.reader(codecs.iterdecode(csvfile, 'utf-8'))
+        report_data = get_course_report_data(term_sis_id)
+        header = report_data.pop(0)
+        for row in csv.reader(report_data):
+            if not len(row):
+                continue
 
-            for row in reader:
-                course = None
-                canvas_id = row[1]
-                term_id = row[4].lstrip('1').lstrip('0')
-                sis_source_id = None if (row[12] == "\\N") else row[12]
-                try:
-                    created_at = parse(row[8]).replace(tzinfo=utc)
-                except ParserError as ex:
-                    pass
+            canvas_course_id = row[0]
+            course_sis_id = row[1]
+            term_sis_id = row[8]
+            course = None
 
-                try:
-                    course = Course.objects.find_course(
-                        canvas_id, sis_source_id)
-                    if course.expiration_date is None:
-                        # Backfill sis course with new attrs
-                        course.course_id = sis_source_id
-                        course.canvas_course_id = canvas_id
-                        course.created_date = created_at
-                        course.expiration_date = \
-                            course.default_expiration_date
+            try:
+                course = Course.objects.find_course(
+                    canvas_course_id, course_sis_id)
+                if course.expiration_date is None:
+                    # Backfill sis course with new attrs
+                    course.course_id = course_sis_id
+                    course.canvas_course_id = canvas_course_id
 
-                        if commit:
-                            course.save()
-                        else:
-                            self._log('UPDATE', course)
+                    # API request to get course.created_at
+                    try:
+                        canvas_course = get_course_by_id(canvas_course_id)
+                        course.created_date = canvas_course.created_at
+                        course.expiration_date = course.default_expiration_date
+                    except DataFailureException as err:
+                        self._log(f'ERROR {canvas_course_id} {course_sis_id}',
+                                  course)
+                        continue
+
+                    if commit:
+                        course.save()
                     else:
-                        if not commit:
-                            self._log('SKIP', course)
+                        self._log('UPDATE', course)
+                else:
+                    if not commit:
+                        self._log('SKIP', course)
 
-                except Course.MultipleObjectsReturned:
-                    self._log(f'ERROR {canvas_id} {sis_source_id}', course)
-                    break
-                except Course.DoesNotExist:
-                    course = Course(
-                        course_id=sis_source_id,
-                        canvas_course_id=canvas_id,
-                        course_type=Course.ADHOC_TYPE,
-                        term_id=terms.get(term_id),
-                        created_date=created_at,
-                        priority=Course.PRIORITY_NONE)
-                    expiration_date = course.default_expiration_date
+            except Course.MultipleObjectsReturned:
+                self._log(f'ERROR {canvas_course_id} {course_sis_id}', course)
+                break
+            except Course.DoesNotExist:
+                course = Course(course_id=course_sis_id,
+                                canvas_course_id=canvas_course_id,
+                                course_type=Course.ADHOC_TYPE,
+                                term_id=term_sis_id,
+                                priority=Course.PRIORITY_NONE)
+
+                # API request to get course.created_at
+                try:
+                    canvas_course = get_course_by_id(canvas_course_id)
+                    course.created_date = canvas_course.created_at
+                    course.expiration_date = course.default_expiration_date
+                except DataFailureException as err:
+                    self._log(f'ERROR {canvas_course_id} {course_sis_id}',
+                              course)
+                    continue
 
                     # Temporary logic for first round of expirations
                     if expiration_date.year == 2023:
