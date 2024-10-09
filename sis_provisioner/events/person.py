6b179069a8abfd748043801d01ebde450a928430
Jim Laney <jlaney@uw.edu>
2024-10-09 09:37:56 -0700

    optionally log person event data

--- sis_provisioner/events/person.py
+++ sis_provisioner/events/person.py
@@ -7,6 +7,7 @@ from sis_provisioner.models.user import User
 from sis_provisioner.models.events import PersonLog
 from uw_sws.models import Person as PersonModel
 import json
+import os
 
 log_prefix = 'PERSON:'
 QUEUE_SETTINGS_NAME = 'PERSON_V1'
@@ -27,8 +28,12 @@ class PersonProcessor(SISProvisionerProcessor):
             queue_settings_name=QUEUE_SETTINGS_NAME, is_encrypted=False)
 
     def process_message_body(self, json_data):
+        if os.getenv('LOG_PERSON_EVENT_DATA'):
+            self.logger.info('Event data: {}'.format(json.dumps(json_data)))
+
         current = json_data['Current']
         previous = json_data['Previous']
+
         net_id = current['UWNetID'] if current else previous['UWNetID']
         if not net_id:
             self.logger.info('{} IGNORE missing uwnetid, uwregid: {}'.format(
