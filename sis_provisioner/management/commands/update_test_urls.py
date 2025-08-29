3c8a96fd1467ec3431d53941704391c3d1e25bde
mike seibel <mikes@washington.edu>
2025-08-29 10:42:36 -0700

    update test urls to lti 1.3 registrations

--- sis_provisioner/management/commands/update_test_urls.py
+++ sis_provisioner/management/commands/update_test_urls.py
@@ -4,108 +4,109 @@
 
 from django.test.utils import override_settings
 from sis_provisioner.management.commands import SISProvisionerCommand
-from sis_provisioner.models.external_tools import ExternalTool
 from sis_provisioner.dao.canvas import (
-    update_external_tool, get_developer_keys, update_developer_key)
-from restclients_core.dao import LiveDAO
+    get_lti_registrations, update_lti_registration)
 from restclients_core.exceptions import DataFailureException
-from logging import getLogger
 import json
+from logging import getLogger
 
 
 logger = getLogger(__name__)
 
+import logging
+logger.setLevel(logging.DEBUG)
 
 class Command(SISProvisionerCommand):
     help = "Update LTI URLs for non-production Canvas instances"
 
-    BLTI_HOSTS = {
-        'canvas': {
-            'prod': 'https://apps.canvas.uw.edu',
-            'test': 'https://test-apps.canvas.uw.edu'},
-        'panopto': {
-            'prod': 'https://panopto-app.uw.edu',
-            'test': 'https://test.panopto-app.uw.edu'},
-        'course-users': {
-            'prod': 'https://course-users.canvas.uw.edu',
-            'test': 'https://test-course-users.canvas.uw.edu'},
-        'course-roster': {
-            'prod': 'https://courseroster.canvas.uw.edu',
-            'test': 'https://test-courseroster.canvas.uw.edu'},
-        'grading-standards': {
-            'prod': 'https://grading-standards.canvas.uw.edu',
-            'test': 'https://test-grading-standards.canvas.uw.edu'},
-        'libguides': {
-            'prod': 'https://libguides.canvas.uw.edu',
-            'test': 'https://test-libguides.canvas.uw.edu'},
-        'infohub': {
-            'prod': 'https://infohub.canvas.uw.edu',
-            'test': 'https://test-infohub.canvas.uw.edu'},
-    }
+    BLTI_HOSTS = [
+        # canvas
+        ('https://apps.canvas.uw.edu',
+         'https://test-apps.canvas.uw.edu'),
+        # panopto
+        ('https://panopto-app.uw.edu',
+         'https://test.panopto-app.uw.edu'),
+        # course-users
+        ('https://course-users.canvas.uw.edu',
+         'https://test-course-users.canvas.uw.edu'),
+        # course-roster
+        ('https://courseroster.canvas.uw.edu',
+         'https://test-courseroster.canvas.uw.edu'),
+        # grading-standards
+        ('https://grading-standards.canvas.uw.edu',
+         'https://test-grading-standards.canvas.uw.edu'),
+        # libguides
+        ('https://libguides.canvas.uw.edu',
+         'https://test-libguides.canvas.uw.edu'),
+        # infohub
+        ('https://infohub.canvas.uw.edu',
+         'https://test-infohub.canvas.uw.edu')
+    ]
+    TEST_LABEL = '(TEST)'
 
     def handle(self, *args, **options):
         self.update_canvas_test()
         self.update_canvas_beta()
         self.update_job()
 
-    def update_blti_urls(self):
-        LiveDAO.pools = {}
-        for service in self.BLTI_HOSTS:
-            prod_host = self.BLTI_HOSTS.get(service).get('prod')
-            test_host = self.BLTI_HOSTS.get(service).get('test')
+    def update_lti_registrations(self):
+        for reg in get_lti_registrations(
+                params={'include': 'overlaid_configuration'}):
 
-            for tool in ExternalTool.objects.get_by_hostname(prod_host):
-                new_config = tool.config.replace(prod_host, test_host)
-                try:
-                    update_external_tool(
-                        tool.account.canvas_id, tool.canvas_id,
-                        json.loads(new_config))
-                except DataFailureException as err:
-                    # 404s OK
-                    if err.status != 404:
-                        raise
+            conf = reg.get('overlaid_configuration', {}) or {}
+            target_link_uri = conf.get('target_link_uri', '') or ''
 
-    def update_developer_keys(self):
-        for key in get_developer_keys():
-            for service in self.BLTI_HOSTS:
-                prod_host = self.BLTI_HOSTS.get(service).get('prod')
-                test_host = self.BLTI_HOSTS.get(service).get('test')
+            for prod_host, test_host in self.BLTI_HOSTS:
+                if target_link_uri.startswith(prod_host):
 
-                tool = key.get('tool_configuration', {}) or {}
-                if (tool.get('target_link_uri', '').startswith(prod_host)):
-                    tool_json = json.dumps(tool).replace(prod_host, test_host)
-                    tool_update = json.loads(tool_json)
+                    logger.info(f"Update LTI URLs: {prod_host}")
 
-                    key_update = {
-                        'tool_configuration': {
-                            'settings': tool_update
-                        },
-                        'developer_key': {
-                            'name': key.get(
-                                'name', f"Key {key['id']}") + ' (TEST)',
-                            'redirect_uris': tool_update.get(
-                                'target_link_uri'),
-                            'scopes': tool.get('scopes', [])
-                        }
-                    }
+                    registration_json = json.dumps({
+                        'name': self._label_test(reg.get('name', '')),
+                        'configuration': {
+                            'title': self._label_test(conf.get('title', ''))
+                        } | (self._property('target_link_uri', conf) |
+                             self._property('oidc_initiation_url', conf) |
+                             self._property('public_jwk_url',  conf) |
+                             self._property('redirect_uris', conf) |
+                             self._property('redirect_uris', conf) |
+                             self._property('scopes', conf) |
+                             self._property('placements', conf) |
+                             self._property('launch_settings', conf))
+                    })
+
+                    registration_update = json.loads(
+                        registration_json.replace(prod_host, test_host))
+
+                    logger.debug(f"{reg.get('id')} --> {registration_update}")
+                    logger.info("Updating "
+                                f"LTI {registration_update['name']}"
+                                f" ({reg['id']})")
 
                     try:
-                        logger.info(f"Updating developer key {key['id']}")
-                        update_developer_key(key['id'], key_update)
+                        update_lti_registration(
+                            reg.get('id'), registration_update)
                     except DataFailureException as err:
-                        # 404s OK
                         logger.error(f"Canvas API {err.status}: {err}")
                         if err.status != 404:
                             raise
 
+    def _label_test(self, name):
+        return name if name.endswith(self.TEST_LABEL) else (
+            f"{name} {self.TEST_LABEL}")
+
+    def _property(self, prop, conf):
+        try:
+            return {prop: conf[prop]}
+        except KeyError:
+            return {}
+
     @override_settings(
         RESTCLIENTS_CANVAS_HOST="https://uw.beta.instructure.com")
     def update_canvas_beta(self):
-        self.update_blti_urls()
-        self.update_developer_keys()
+        self.update_lti_registrations()
 
     @override_settings(
         RESTCLIENTS_CANVAS_HOST="https://uw.test.instructure.com")
     def update_canvas_test(self):
-        self.update_blti_urls()
-        self.update_developer_keys()
+        self.update_lti_registrations()
